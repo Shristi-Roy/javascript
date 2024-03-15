29bb9568423b8195113e13558775857ff4efd43a
Shristi-Roy <122510934+Shristi-Roy@users.noreply.github.com>
2024-03-15 03:54:46 +0000

    datatypes

---
+++ 01_basics/02_Datatype.js
@@ -0,0 +1,29 @@
+"use Strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs not browser
+
+console.log(3+3) // code retadibility should be high
+
+
+console.log("shristi") // don't use be this type of console.log
+
+let name = "shristi";
+let age = 18;
+let isLoggedIn = false;
+
+// Primitive DataType
+/*
+Number = 2 to the power 
+Boolean => true/false
+null => standlone value
+undefined
+BigInt
+Symbol = unique
+String = " "
+*/
+
+console.log(typeof "Shristi")
+console.log(typeof isLoggedIn)
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
